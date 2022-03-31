98690b3a2a480238a2e9dee5a078cdab8c23dc9d
Huy <nguyenhoaihuy92@gmail.com>
2022-03-30 22:25:35 -0700

    first commit

---
+++ player.py
@@ -0,0 +1,12 @@
+from const import *
+from s4vsprite import s4vSprite
+from spritesheet import Character
+
+class Player(Character):
+    def __init__(self,x,y,sprites_dir,scale):
+        super().__init__(x,y,sprites_dir,scale)
+        
+        
+
+
+    
